affb7d8e7eee544cbad7737d7fcc6d55731452b4
Mark Anderson <mra@ubuntu.(none)>
2012-12-04 17:53:13 -0500

    second commit

---
+++ hello.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  cout << "Hello World!" << endl;
+  int yo= 0;
+  yo=yo+1;
+  yo=yo*2;
+  float ho[9];
+  ho[0]=0;
+  ho[1]=1;
+  ho[2]=2;
+  ho[3]=3;
+  ho[4]=4;
+  ho[5]=5;
+  ho[6]=6;
+  ho[7]=7;
+  ho[8]=8;
+
+  return 0;
+}
+
